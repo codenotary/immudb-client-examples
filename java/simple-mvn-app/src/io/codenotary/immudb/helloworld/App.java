a841162fd21ef2544099eb27aab9a2bfcdb2124a
Giacomo Russo <giako@giako.dev>
2020-11-17 15:55:36 +0100

    feat: add samples for immudb4j 0.2.0
    history sample, scan sample, zadd/zscan sample.

--- java/simple-mvn-app/src/io/codenotary/immudb/helloworld/App.java
+++ java/simple-mvn-app/src/io/codenotary/immudb/helloworld/App.java
@@ -29,7 +29,7 @@ import io.codenotary.immudb4j.crypto.VerificationException;
 
 public class App {
 
-	public static void main(String args[]) {
+	public static void main(String[] args) {
 		
 		ImmuClient client = null;
 		
@@ -40,13 +40,14 @@ public class App {
 			client = ImmuClient.newBuilder().setServerUrl("localhost").setServerPort(3322).setRootHolder(rootHolder).build();
 			
 			client.login("immudb", "immudb");
-			
+
+			client.useDatabase("defaultdb");
 			
 			client.set("hello", "immutable world!".getBytes());
 			
 			byte[] v = client.safeGet("hello");
 			
-			System.out.format("(%s, %s)", "hello", new String(v));
+			System.out.format("(%s, %s)%n", "hello", new String(v));
 			
 
 			// Multi-key operations
@@ -61,7 +62,7 @@ public class App {
 		    client.setAll(kvList);
 
 
-		    List<String> keyList = new ArrayList<String>();
+		    List<String> keyList = new ArrayList<>();
 
 		    keyList.add("k123");
 		    keyList.add("k321");
@@ -73,14 +74,43 @@ public class App {
 		        byte[] key = kv.getKey();
 		        byte[] value = kv.getValue();
 
-		        System.out.format("(%s, %s)", new String(key), Arrays.toString(value));
+		        System.out.format("(%s, %s)%n", new String(key), Arrays.toString(value));
 		    }
-			
-			
+
+		    // History operations
+			client.set("history", new byte[]{1, 2, 3});
+			client.set("history", new byte[]{3, 2, 1});
+
+			List<KV> history = client.history("history");
+
+			System.out.format("History of 'history', record 1: (%s, %s)%n", new String(history.get(0).getKey()),
+					Arrays.toString(history.get(0).getValue()));
+			System.out.format("History of 'history', record 2: (%s, %s)%n", new String(history.get(1).getKey()),
+					Arrays.toString(history.get(1).getValue()));
+
+			// Scan operations
+            client.set("scanFirstKey", new byte[] {1, 2, 3});
+			client.set("scanSecondKey", new byte[] {4, 5, 6});
+
+			List<KV> scan = client.scan("scan", "", 10, false, false);
+			System.out.format("Scan results of 'scan', record 1: (%s, %s)%n", new String(scan.get(0).getKey()),
+					Arrays.toString(scan.get(0).getValue()));
+			System.out.format("Scan results of 'scan', record 2: (%s, %s)%n", new String(scan.get(1).getKey()),
+					Arrays.toString(scan.get(1).getValue()));
+
+			// Zadd, zscan operations
+			client.zAdd("newSet", "scanFirstKey", 2);
+			client.zAdd("newSet", "scanSecondKey", 1);
+
+			List<KV> zScan = client.zScan("newSet", "", 10, false);
+			System.out.format("Scan results of 'zscan', record 1: (%s, %s)%n", new String(zScan.get(0).getKey()),
+					Arrays.toString(zScan.get(0).getValue()));
+			System.out.format("Scan results of 'zscan', record 2: (%s, %s)%n", new String(zScan.get(1).getKey()),
+					Arrays.toString(zScan.get(1).getValue()));
 		} catch (IOException e) {
 			e.printStackTrace();
 		} catch (VerificationException e) {
-			// TODO: tampering detected!
+			// Tampering detected!
 			// This means the history of changes has been tampered
 			e.printStackTrace();
 			System.exit(1);
