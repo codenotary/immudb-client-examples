fe314735691ba32ea4528e34325579f7402dc196
Jeronimo Irazabal <jeronimo.irazabal@gmail.com>
2022-06-02 16:57:47 -0300

    upgrade sample with latest immudb4j release v0.10.2
    Signed-off-by: Jeronimo Irazabal <jeronimo.irazabal@gmail.com>

--- java/simple-app/src/main/java/io/codenotary/immudb/helloworld/App.java
+++ java/simple-app/src/main/java/io/codenotary/immudb/helloworld/App.java
@@ -1,5 +1,5 @@
 /*
-Copyright 2021 CodeNotary, Inc. All rights reserved.
+Copyright 2022 CodeNotary, Inc. All rights reserved.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -55,9 +55,10 @@ public class App {
                 // Getting it back, by key (in a verified way
                 // that reports any tampering if it happened).
                 Entry entry = client.verifiedGet(key);
-                System.out.format("(%s, %s)%n", key, new String(entry.kv.getValue()));
 
-            } catch (VerificationException | CorruptedDataException e) {
+                byte[] value = entry.getValue();
+                System.out.format("(%s, %s)\n", key, new String(value));
+            } catch (VerificationException e) {
                 // VerificationException means Data Tampering detected!
                 // This means the history of changes has been tampered.
                 e.printStackTrace();
@@ -74,31 +75,29 @@ public class App {
             keyList.add(key2);
 
             // A multi-key read.
-            List<KV> result = client.getAll(keyList);
+            List<Entry> result = client.getAll(keyList);
 
-            for (KV kv : result) {
-                byte[] k = kv.getKey();
-                byte[] v = kv.getValue();
+            for (Entry e : result) {
+                byte[] k = e.getKey();
+                byte[] v = e.getValue();
 
-                System.out.format("(%s, %s)%n", new String(k), Arrays.toString(v));
+                System.out.format("(%s, %s)\n", new String(k), Arrays.toString(v));
             }
 
             // History operations.
             client.set("history", new byte[]{1, 2, 3});
             client.set("history", new byte[]{3, 2, 1});
 
-            List<KV> history = client.history("history", 10, 0, false);
+            List<Entry> history = client.history("history", 10, 0, false);
 
-            System.out.format("History of 'history', entry 1: (%s, %s)%n",
+            System.out.format("History of 'history', entry 1: (%s, %s)\n",
                     new String(history.get(0).getKey()),
                     Arrays.toString(history.get(0).getValue())
             );
-            System.out.format("History of 'history', entry 2: (%s, %s)%n",
+            System.out.format("History of 'history', entry 2: (%s, %s)\n",
                     new String(history.get(1).getKey()),
                     Arrays.toString(history.get(1).getValue())
             );
-
-            TxMetadata txMd;
 
             // Scan operations.
             String prefix = "myKey";
@@ -106,18 +105,19 @@ public class App {
             key2 = prefix + "2";
 
             client.set(key1, new byte[]{1, 2, 3});
-            txMd = client.set(key2, new byte[]{4, 5, 6});
+
+            client.set(key2, new byte[]{4, 5, 6});
 
             // scan is usually done by a prefix.
             // Of course, we can scan by a complete key name.
-            List<KV> scan = client.scan(prefix, txMd.id, 2, false);
+            List<Entry> scan = client.scan(prefix, 2, false);
 
-            System.out.format("Scan results of '%s', entry 1: (%s, %s)%n",
+            System.out.format("Scan results of '%s', entry 1: (%s, %s)\n",
                     prefix,
                     new String(scan.get(0).getKey()),
                     Arrays.toString(scan.get(0).getValue())
             );
-            System.out.format("Scan results of '%s', entry 2: (%s, %s)%n",
+            System.out.format("Scan results of '%s', entry 2: (%s, %s)\n",
                     prefix,
                     new String(scan.get(1).getKey()),
                     Arrays.toString(scan.get(1).getValue())
@@ -125,20 +125,21 @@ public class App {
 
             // zAdd, zScan operations.
             String set = "mySet";
-            client.zAdd(set, 2, key1);
-            txMd = client.zAdd(set, 1, key2);
+            client.zAdd(set, key1, 2);
+
+            client.zAdd(set, key2, 2);
 
             // Here, we do a zScan providing the `sinceTxId` which should be
             // the latest transaction id we are interested in being considered.
-            List<KV> zScan = client.zScan(set, txMd.id, 10, false);
+            List<ZEntry> zScan = client.zScan(set, 10, false);
 
-            System.out.format("Results of 'zScan', record 1: (%s, %s)%n",
+            System.out.format("Results of 'zScan', record 1: (%s, %s)\n",
                     new String(zScan.get(0).getKey()),
-                    Arrays.toString(zScan.get(0).getValue())
+                    Arrays.toString(zScan.get(0).getEntry().getValue())
             );
-            System.out.format("Results of 'zScan', record 2: (%s, %s)%n",
+            System.out.format("Results of 'zScan', record 2: (%s, %s)\n",
                     new String(zScan.get(1).getKey()),
-                    Arrays.toString(zScan.get(1).getValue())
+                    Arrays.toString(zScan.get(1).getEntry().getValue())
             );
 
         } catch (IOException | CorruptedDataException e) {
