a6755b9cd949e8e740156abe73f9bafbcc4388c3
Jeronimo Irazabal <jeronimo.irazabal@gmail.com>
2023-09-26 13:04:14 +0200

    bump immudb version in tx by id sample
    Signed-off-by: Jeronimo Irazabal <jeronimo.irazabal@gmail.com>

--- go/transaction-by-id/main.go
+++ go/transaction-by-id/main.go
@@ -26,7 +26,9 @@ import (
 
 // Simple app using official go sdk for immudb
 
-// go run main.go
+// go mod tidy
+// go build
+// ./txbyid
 
 func main() {
 	// even though the server address and port are defaults, setting them as a reference
